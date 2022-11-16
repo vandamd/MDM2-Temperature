8b77574f96e29e9f75a595fc30174720025a9817
Mikolaj Szymczak <98821923+MikolajSzymczak@users.noreply.github.com>
2022-11-16 13:31:20 +0000

    plots graph now but I think the equation is wrong?

--- temperatureInRoom.py
+++ temperatureInRoom.py
@@ -7,51 +7,51 @@ def thermalCapacitanceCalculator(p,v,c_p):
     C = p*v*c_p
     return C
 
+
+#def conductionResistanceCalculator(L_t, A, k): 
+#    """conduction resistance = thickness of surface / thermal conductivity * area of the surface"""
+#    R_cond = L_t/k*A
+#    return R_cond
+
+#def convectiveResistanceCalculator(L_t, A, k): #same as conduction, don't really understand??
+#    """convection resistance = thickness of surface / thermal conductivity * area of the surface"""
+#   R_conv = L_t/(A*k)
+#    return R_conv
+
+#define constants
+k_wall = 0.126
+k_air = 0.02435
+Lt_wall = 0.23
+Lt_air = 1
+A_wall = 1
+A_air = 1
+C_wall = thermalCapacitanceCalculator(600.5, 0.29*1*1*4, 2300)
 C_air = thermalCapacitanceCalculator(1.276, 1, 1.006)
-C_walls = thermalCapacitanceCalculator(600.5, 0.29*1*1*4, 2300)
 
-def conductionResistanceCalculator(L_t, A, k): 
-    """conduction resistance = thickness of surface / thermal conductivity * area of the surface"""
-    R_cond = L_t/k*A
-    return R_cond
+def airTemperatureChangeBasic(T,t,k_air,Lt_air, A_air, C_air):
+    """energy rate of change = thermal conductivity * area of the surface / thickness of surface
+    rate of change of temperature = energy rate of change / thermal capacitance""" 
+    dqdt = (k_air*A_air/Lt_air) #equation for q, the heat transfer
+    dTdt = dqdt/C_air #from Vandam's Equation (changing room temperature equation)
+    return dTdt
 
+def wallTemperatureChangeBasic(T,t,k_wall,Lt_wall, A_wall, C_wall):
+    dqdt = (k_wall*A_wall/Lt_wall) #equation for q, the heat transfer
+    dTdt = dqdt/C_wall #from Vandam's Equation (changing room temperature equation)
+    return dTdt
 
-def convectiveResistanceCalculator(L_t, A, k): #same as conduction, don't really understand??
-    """convection resistance = thickness of surface / thermal conductivity * area of the surface"""
-    R_conv = L_t/(A*k)
-    return R_conv
-
-def findingEnergyChangeBasic(t,k,L_t,A):
-    """energy rate of change = thermal conductivity * area of the surface / thickness of surface""" 
-    dqdt = (k*A/L_t) #equation for q, the heat transfer
-    return dqdt
-
-
-"""
-def findingEnergyChangeWithResistance(t,k,L_t,A):
-    k = 10 #thermal conductivity
-    A = 1 #area of the surface
-    L_t = 25 #thickness of surface
-    dqdt = (k*A/L_t) #equation for q, the heat transfer
-    return dqdt
-"""
-
-"""
-k = [-2, 0, 1] #positive if trying to heat up room and negative if not
+initialTemp = [25]
 t = np.linspace(0,20,200)
-woodEnergyChange = energyChange(t,0.126, 1, 0.23) #for a 1x1x1 room
-result = odeint(energyChange,k,t,args=(k,))
+
+airResult = odeint(airTemperatureChangeBasic, initialTemp, t, args = (k_air, Lt_air, A_air, C_air))
+wallResult = odeint(wallTemperatureChangeBasic, initialTemp, t, args = (k_wall, Lt_wall, A_wall, C_wall))
+#outsideResult = odeint(outsideTemperatureChange, initalTemp, t,)
 
 fig,ax = plt.subplots()
-ax.plot(t,result[:,0],label='k=-1')
-ax.plot(t,result[:,1],label='k=0')
-ax.plot(t,result[:,2],label='k=1')
+ax.plot(t,airResult,label='heater->air')
+ax.plot(t,wallResult,label='air->wall')
+#ax.plot(t,outsideResult,label='wall->outside')
 ax.legend()
 ax.set_xlabel('t')
 ax.set_ylabel('T')
 plt.show()
-"""
-
-"""Thermal capacitance C = mc_p"""
-"""thermal behaviour is mainly influenced by convection and conduction"""
-"""formula for heat transfer, q_cv: (kA/L_t)*(dt) where k is thermal conductivity, A is the area and L_t is the thickness in m"""
