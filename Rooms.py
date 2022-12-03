e7f93248de08180e8db1d4530191d2556053643d
Alastair Kirkwood <rw20805@bristol.ac.uk>
2022-12-03 16:41:38 +0000

    changing code to match name of file

--- Rooms.py
+++ Rooms.py
@@ -24,39 +24,37 @@ def thermalCapacitanceCalculator(p,V,c_p):
 # ========================Variable Initialisation==============================                      
 T10 = 15                                        # Initial temp of Room 1                   
 T20 = 20                                        # Initial temp of Room 2
-T30 = 12                                        # Initial temp of Room 3
+T30 = 10                                        # Initial temp of Room 3
 To0 = 0                                         # Initial temp of outside the house
-N = 1                                        # Number of hours in simulation
+N = 24                                        # Number of hours in simulation
 Xn = 5                                          # Number of points plotted per minute 
-t = np.linspace(0, N*(60), (Xn*N*(60))+1) 
+t = np.linspace(0, N*(60), (Xn*N*(60))+1)       
 k_wall = 0.038
-k_air = 0.02435
-k_window = 0.96                                 # this is the value for glass, apparently lower for windows, needs looking into
+k_air = 0.02435                                 
 Cp_air = 1005
 Cv_air = 718
 #alternative_outside_temp1 = -(1/144)*math.pi*math.sin((math.pi*t)/1440)     # goes to -20 after 1 day
 #alternative_outside_temp2 = -(1/9)*math.sin(t/90)                           # goes to -20 after 283 minutes
+#alternative_outside_temp3 = (7/240)*math.pi*math.sin((math.pi*t)/120)
 # ========================Dimensional variables==============================
 L_room1 = 10
 L_room2 = 10 
 L_room3 = 10
 L_wall = 10
-L_window = 0.7
 
 W_room1 = 10
 W_room3 = 10
 W_room2 = 12.5
-Thickness_wall = 0.23
-Thickness_window = 0.00238                 # not sure if correct, different companies say different things
+Thickness_wall = 0.23               
 
 H_room1 = 2
 H_room2 = 2
 H_room3 = 2
 H_wall = 2
-H_window = 0.7
 
-A_Window = AreaCalc(L_window,H_window)
-
+A_room1 = AreaCalc(L_room1,W_room1)
+A_room2 = AreaCalc(L_room2,W_room2)
+A_room3 = AreaCalc(L_room3,W_room3)
 V_room1 = VolumeCalc(L_room1,W_room1,H_room1)
 V_room2 = VolumeCalc(L_room2,W_room2,H_room2)
 V_room3 = VolumeCalc(L_room3,W_room3,H_room3)
@@ -89,12 +87,10 @@ def Heat_model(y,t):
      of each equation is equal to the heat flux (which is flow of energy over time, dQdt)'''
 
     # Net heat flow for every room                        
-    dQ1dt = ((2*A_Wall_1_o)*(k_wall/Thickness_wall)*(To-T1)) + ((A_Wall_1_o - A_Window)*(k_wall/Thickness_wall)*(To-T1)) + (
-        (A_Window)*(k_window/Thickness_window)*(To-T1)) + ((A_Wall_1_2)*(k_wall/Thickness_wall)*(T2-T1))  
-
-    dQ2dt = ((A_Wall_1_2)*(k_wall/Thickness_wall)*(T1-T2)) + ((A_Wall_3_2)*(k_wall/Thickness_wall)*(T3-T2)) + ((2*A_Wall_2_o)*(k_wall/Thickness_wall)*(To-T2)) 
-    dQ3dt = ((A_Wall_3_2)*(k_wall/Thickness_wall)*(T2-T3)) + ((3*A_Wall_3_o)*(k_wall/Thickness_wall)*(To-T3)) 
-    dQodt = (7/240)*math.pi*math.sin((math.pi*t)/120)
+    dQ1dt = ((A_Wall_1_2)*(k_wall/Thickness_wall)*(T2-T1)) + ((3*A_Wall_1_o)*(k_wall/Thickness_wall)*(To-T1)) + ((A_room1)*(k_wall/Thickness_wall)*(To-T1))
+    dQ2dt = ((A_Wall_1_2)*(k_wall/Thickness_wall)*(T1-T2)) + ((A_Wall_3_2)*(k_wall/Thickness_wall)*(T3-T2)) + ((2*A_Wall_2_o)*(k_wall/Thickness_wall)*(To-T2)) + ((A_room3)*(k_wall/Thickness_wall)*(To-T2))
+    dQ3dt = ((A_Wall_3_2)*(k_wall/Thickness_wall)*(T2-T3)) + ((3*A_Wall_3_o)*(k_wall/Thickness_wall)*(To-T3)) + ((A_room3)*(k_wall/Thickness_wall)*(To-T3))
+    dQodt = 0
     # rate of change of temperature for each room
     dT1dt = (dQ1dt/C_room1)*60
     dT2dt = (dQ2dt/C_room2)*60
@@ -119,9 +115,8 @@ plt.plot(t, T2, 'b', lw = 2, label = 'Middle room temp')
 plt.plot(t, T3, 'r', lw = 2, label = 'Right room temp') 
 plt.plot(t, To, 'm', lw = 2, label = 'outside temperature')
 plt.title('Temperature over time')
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-          fancybox=True, shadow=True, ncol=5)
-plt.xlabel('Time(minutes)');
+plt.legend()
+plt.xlabel('Time(minutes)')
 plt.ylabel('Temperature ($^\circ$C)')
 plt.grid()
 plt.show()
